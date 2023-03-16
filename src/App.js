6a3c6b2b7f827c094c9da2cedd459d896d6f85ad
olyaturova <96010631+olyaturova@users.noreply.github.com>
2023-03-16 10:10:04 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getAllMeals, addMeal, editMeal, deleteMeal } from './FetchMeal';
+import { MyMeals } from './MyMeals';
+
+
+function App() {
+
+const [myMeal, setMeal] = useState([]);
+const [title, setTitle] = useState("");
+const [editing, setEditing] = useState(false);
+const [mealId, setMealId] = useState("")
+
+useEffect(() => {
+  getAllMeals(setMeal)
+}, [])
+
+const updatingInInput = (_id, title) => {
+  setEditing(true)
+  setTitle(title)
+  setMealId(_id)
+}
+
+  return (
+    <div>
+      <h1>Meal plan</h1>
+
+      <input
+      type="text"
+      placeholder="Add a meal"
+      value = {title}
+      onChange = {(e) => setTitle(e.target.value)}
+      />
+
+      <button onClick=
+      {editing ? () => editMeal(mealId, title, setTitle, setMeal, setEditing) : () => addMeal(title, setTitle, setMeal)}>
+      {editing ? "Edit" : "Add"}
+        </button>
+
+      {myMeal.map((meal) => <MyMeals text={meal.title} key={meal._id}
+      updatingInInput={() => updatingInInput(meal._id, meal.title)}
+      deleteMeal={() => deleteMeal(meal._id, setMeal)}/>
+      )}
+    </div>
+  );
+}
+
+export default App;
